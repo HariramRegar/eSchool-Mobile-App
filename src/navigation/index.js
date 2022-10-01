8f8a8b5f39565a83a5463fe8a1f80d37bd58e451
Hariram Regar <regarhariram836@gmail.com>
2022-10-01 12:19:30 +0530

    added navigation screen header

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeBottomTabNavigator from './homeBottomTabNavigator';
 import Blank from '../screens/Blank';
 import Home from '../screens/Home';
 import Notifications from '../screens/Notifications';
@@ -19,16 +18,16 @@ const RootNavigation = () => {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerShown: false,
+          headerShown: true,
         }}
       >
-        <Stack.Screen name="loginoptions" component={LoginOptions} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="blank" component={Blank} />
-        <Stack.Screen name='notifications' component={Notifications} />
-        <Stack.Screen name='stats' component={Stats} />
-        <Stack.Screen name='results' component={Results} />
-        <Stack.Screen name='login' component={Login} />
+        <Stack.Screen name="loginoptions" component={LoginOptions} options={{ title: 'Welcome', headerShown: false }} />
+        <Stack.Screen name="home" component={Home} options={{ title: 'Home' }} />
+        <Stack.Screen name="blank" component={Blank} options={{ title: 'Blank' }} />
+        <Stack.Screen name='notifications' component={Notifications} options={{ title: 'Notifications' }} />
+        <Stack.Screen name='stats' component={Stats} options={{ title: 'Stats' }} />
+        <Stack.Screen name='results' component={Results} options={{ title: 'Results' }} />
+        <Stack.Screen name='login' component={Login} options={{ title: 'Login' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
